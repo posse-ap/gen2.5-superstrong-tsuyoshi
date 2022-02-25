bd4a6c8eecfefdefb82c6dc22bf87578088b6bb6
張致豪 <chohtsuyoshi@chouitasugounoMacBook-Air.local>
2022-02-25 09:50:16 +0900

    提出

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ let No1Selection = ['たかなわ','たかわ','こうわ']
 let No2Selection = ['かめいど','かめど','かめと']
 let No3Selection = ['こうじまち','かゆまち','おかとまち']
 let No4Selection = ['おなりもん','おかどもん','ごせいもん']
-let No5Selection = ['とどりき','たたら','たたりき']
+let No5Selection = ['とどろき','たたら','たたりき']
 let No6Selection = ['しゃくじい','せきこうい','いじい']
 let No7Selection = ['ぞうしき','ざっしき','ざっしょく']
 let No8Selection = ['おかちまち','みとちょう','ごしろちょう']
@@ -40,20 +40,34 @@ let No9Selection = ['ししぼね','ろっこつ','しこね']
 let No10Selection= ['こぐれ','こばく','こしゃく']
 let Selections   = [No1Selection,No2Selection,No3Selection,No4Selection,No5Selection,
   No6Selection,No7Selection,No8Selection,No9Selection,No10Selection]
+let answer = ['たかなわ','かめいど','こうじまち','おなりもん','とどろき','しゃくじい','ぞうしき','おかちまち','ししぼね','こぐれ']
 
+// });
 
 
 for(let i = 0; i < 10; i ++) {
+  let li = [`<li id="correct_${i}" onclick="button(${i})">${Selections[i][0]}</li>`,
+  `<li id="wrong_${i}" onclick="buttons(${i})">${Selections[i][1]}</li>`,
+  `<li id="miss_${i}" onclick="option(${i})">${Selections[i][2]}</li>`]
+    
+    // Selections.(element => {
+      // const shuffle = ([...Selections]) => {
+        for (let k = li.length - 1; k >= 0; k--) {
+          const j = Math.floor(Math.random() * (k + 1));
+          [li[k], li[j]] = [li[j], li[k]];
+          console.log(li)
+          // return li;
+      }
   const quiz = `<h2>${i + 1}、この地名はなんて読む？</h2>`
 + `<img src="./img/photo${i}.png">`
-+ `<ul><li id="correct_${i}" onclick="button(${i})">${Selections[i][0]}</li><li id="wrong_${i}" onclick="buttons(${i})">${Selections[i][1]}</li><li id="miss_${i}" onclick="option(${i})">${Selections[i][2]}</li></ul>`
++ `<ul>${li[0]}${li[1]}${li[2]}</ul>`
 + `<div id="answer_${i}" class="answer_invisible">
 <p class="congratulation">正解!</p>
-<p>正解は${Selections[i][0]}ですう</p>
+<p>正解は${answer[i]}ですう</p>
 </div>`
 + `<div id="mistake_${i}" class="answer_invisible">
 <p class="bad">不正解!</p>
-<p>正解は${Selections[i][0]}ですう</p>
+<p>正解は${answer[i]}ですう</p>
 </div>`
 const quiz_box=document.getElementById("quiz_box");
 quiz_box.insertAdjacentHTML('beforebegin',quiz);
@@ -77,8 +91,3 @@ function option(ro){
   document.getElementById(`correct_${ro}`).classList.add("blue");
   document.getElementById(`wrong_${ro}`).classList.add ("pointer_events");
 }
-
-// aaa
-
-
-
