923b105691eea03eb7ded7651651c555f94d8a80
張致豪 <chohtsuyoshi@chouitasugounoMacBook-Air.local>
2022-03-24 00:11:07 +0900

    一旦提出

---
+++ main.js
@@ -0,0 +1,10 @@
+var data = new google.visualization.DataTable();
+		data.addColumn('string');
+		data.addColumn('number', '田中');
+		data.addColumn('number', '山根');
+
+		data.addRows([
+			["2020-01", 14, 28],
+			["2020-02", 16, 20],
+			["2020-03", 31, 15]
+		]);
