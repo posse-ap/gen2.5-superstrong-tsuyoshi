352f83a37349f461fc939ebedea6e94a03db284e
張致豪 <chohtsuyoshi@chouitasugounoMacBook-Air.local>
2022-04-21 00:18:02 +0900

    ついに完成

--- main.js
+++ main.js
@@ -1,10 +1,53 @@
-var data = new google.visualization.DataTable();
-		data.addColumn('string');
-		data.addColumn('number', '田中');
-		data.addColumn('number', '山根');
+// const mainButton = document.getElementById("mainButton")
+const modal = document.getElementById("modal");
+const checkingPage = document.getElementById("checkingPage");
+const close = document.getElementById("close");
+const loadingPage = document.getElementById("loadingPage");
+const successPage = document.getElementById("successPage");
+const calendar = document.getElementById("calendar");
+const mainButton =({click,checking,bodyElement})=>{
+	click.classList.add(`active`);
+	checking.classList.add(`active`);
+	bodyElement.classList.add(`open-overlay`)
+	// load.classList.add(`active`);
+	// successPage.classList.add(`active`);
+	console.log(checking);
+}
 
-		data.addRows([
-			["2020-01", 14, 28],
-			["2020-02", 16, 20],
-			["2020-03", 31, 15]
-		]);
+const closeButton =({click,checking,bodyElement,document})=>{
+	click.classList.remove(`active`);
+	checking.classList.remove(`active`);
+	bodyElement.classList.remove(`open-overlay`)
+	document.classList.remove(`active`)
+	// load.classList.add(`active`);
+	// successPage.classList.add(`active`);
+	console.log(checking);
+}
+
+const checkbox = document.getElementById("checkbox1")
+// checkbox.addEventListener(`click`,function(){
+// 	checkbox.classList.toggle(`language-lists1`)
+// })
+const checkboxColor =({check})=>{
+	check.classList.toggle(`language-lists1`);
+}
+
+const twitter = document.getElementById("twitterButton1")
+
+const checkColor=({clicking})=>{
+	clicking.classList.toggle(`twitter-button`)
+}
+const loadingOpen = ({modalElement,picture,completion})=>{
+	modalElement.classList.remove(`active`);
+	picture.classList.add(`active`);
+	setTimeout(() => {
+		completion.classList.add(`active`)
+		picture.classList.remove(`active`)
+	}, 5000);
+}
+
+
+
+
+
+
